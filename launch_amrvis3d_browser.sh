989217f73349e8531990041ddb068e782a903adc
Ben Wibking <ben@wibking.com>
2025-05-25 13:54:41 -0400

    update launch scripts

--- launch_amrvis3d_browser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-## Wrapper script to launch amrvis3d and connect with a browser-based X11 server
-MOUNT_DIR=${1:-`pwd`}
-
-docker run -p 8080:8080 --platform linux/amd64 -v $MOUNT_DIR:/home/vscode/data ghcr.io/benwibking/amrvis-container:latest
