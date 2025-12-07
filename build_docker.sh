97515e3b8ccae231cc92cbda9140af03078ad8dc
Ben Wibking <ben@wibking.com>
2025-12-07 18:27:54 -0500

    update container URL

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-docker build --platform linux/amd64 --tag ghcr.io/benwibking/amrvis-container:latest .
+docker build --platform linux/amd64 --tag ghcr.io/amrex-codes/amrvis-container:latest .
